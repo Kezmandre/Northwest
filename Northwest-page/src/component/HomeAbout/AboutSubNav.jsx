6b489cc508d0b09348cb1646146ef63fb416402c
Quadri <kazeemquadri75@gmail.com>
2024-09-30 22:57:48 +0100

    aboutSubNav

--- Northwest-page/src/component/HomeAbout/AboutSubNav.jsx
+++ Northwest-page/src/component/HomeAbout/AboutSubNav.jsx
@@ -4,7 +4,7 @@ import { TiArrowSortedUp } from "react-icons/ti";
 const AboutSubNav = () => {
   return (
     <div className="w-[95%] mx-auto ">
-      <div className="flex justify-start gap-8 items-start w-full mt-8">
+      <div className="flex justify-between gap-8 items-start w-full mt-8">
         <div className="w-[400px]">
           <h2 className="pb-4 text-black">About Us</h2>
           <p className="text-black text-[14px] text-justify font-normal">
@@ -13,20 +13,41 @@ const AboutSubNav = () => {
             crude oil and refined petroleum products
           </p>
         </div>
-        <div className="">
-          <h2 className="pb-4">Company Profile</h2>
+        <div className="hover:text-[#0B6A42]">
+          <h2 className="pb-4 text-black">Company Profile</h2>
           <ul>
-            <span className="flex justify-center text-black text-[14px]  font-normal items-center gap-2">
+            <span className="flex justify-start text-black hover:text-[#0B6A42] text-[14px] mb-4  font-normal items-center gap-2">
               {" "}
               <TiArrowSortedUp />
-              <li>Premium Motor Spirit (PMS)</li>
+              <li>Mission and Vision</li>
+            </span>
+
+            <span className="flex justify-start text-black hover:text-[#0B6A42] text-[14px] pb-4  font-normal items-center gap-2">
+              {" "}
+              <TiArrowSortedUp />
+              <li>Our Story</li>
+            </span>
+            <span className="flex justify-start text-black hover:text-[#0B6A42] text-[14px]   font-normal items-center gap-2">
+              {" "}
+              <TiArrowSortedUp />
+              <li>Our Milestone</li>
             </span>
           </ul>
         </div>
         <div className="">
-          <h2>Management</h2>
-          <p>Our Team</p>
-          <h2>HSE Management</h2>
+          <h2 className="pb-4 text-black">Management</h2>
+          <ul className="hover:text-[#0B6A42]">
+            <span className="flex justify-start text-black hover:text-[#0B6A42] text-[14px] mb-4  font-normal items-center gap-2">
+              {" "}
+              <TiArrowSortedUp />
+              <li>Our Team</li>
+            </span>
+            <span className="flex justify-start text-black hover:text-[#0B6A42] text-[14px] mb-4  font-normal items-center gap-2">
+              {" "}
+              <TiArrowSortedUp />
+              <li>HSE Commitment</li>
+            </span>
+          </ul>
         </div>
       </div>
     </div>
